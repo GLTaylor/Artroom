fc4011d7c4a3ab5fea08b0260af90f168f4b3611
Ginny Wan <zctywan@ucl.ac.uk>
2018-08-20 16:48:38 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,76 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hanging.delete_all
+Artwork.delete_all
+Artist.delete_all
+User.delete_all
+
+User.create!(
+  [
+    {
+      email: "gin@gmail.com",
+      user_name: "ginmule",
+      password: "123456",
+    },
+    {
+     email: "camille@hotmail.com",
+    user_name: "camilleraiser",
+     password: "123456",
+   },
+    {
+     email: "taylor@hotmail.com",
+     user_name:  "GLTaylor",
+     password: "123456",
+   }
+ ]
+ )
+
+Artist.create! (
+    {
+      name: "Ren Hang",
+      nationality: "Chinese",
+      biography: "",
+      birthday: "1987",
+      deathday: "2017"
+    },
+    {
+      name: "Ren Hang",
+      nationality: " ",
+      biography: " ",
+      birthday: " ",
+      deathday: " ",
+      location: " "
+   }
+)
+
+  one = Artwork.create!(
+    title: "",
+    description: "",
+    date: " ",
+    category: " ",
+    medium: " ",
+    mood: "Wild",
+    interest: "Eroticism",
+    image: " ",
+    user: User.all.sample
+  )
+
+  one.remote_photo_url = "_____"
+  one.save!
+
+  two = Artwork.create!(
+    title: "",
+    description: "",
+    date: " ",
+    category: " ",
+    medium: " ",
+    mood: " ",
+    interest: " ",
+    image: " ",
+    user: User.all.sample
+  )
+
+  two.remote_photo_url = "____"
+  two.save!
