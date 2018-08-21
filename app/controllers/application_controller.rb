5459f2af9fad0f8ebd8f65ca99680bc47c0ddf22
camille raiser <camille.raiser@hotmail.fr>
2018-08-21 11:51:25 +0200

    add first setup to pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, unless: :skip_pundit?
+  after_action :verify_policy_scoped, unless: :skip_pundit?
+
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
 end
