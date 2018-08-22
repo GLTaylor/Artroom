b7a4987258239ba9be5a4e2a6a1fbc939f56c62f
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-08-22 17:05:11 +0200

    quiz function complete, works also when a user simply navigates to an artwork show page

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,21 +1,19 @@
 class ArtworksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
-
-
   def show
     if params[:mood].present? && params[:interest].present?
       @artworks = Artwork.search_by_mood(params[:mood]).search_by_interest(params[:interest])
-      authorize @artworks
+      sample_authorize
     elsif params[:interest].present? && params[:mood] == ""
       @artworks = Artwork.search_by_interest(params[:interest])
-      authorize @artworks
+      sample_authorize
     elsif params[:mood].present? && params[:interest] == ""
       @artworks = Artwork.search_by_mood(params[:mood])
-      authorize @artworks
+      sample_authorize
     elsif params[:mood] == "" && params[:interest] == ""
       @artworks = Artwork.all
-        authorize @artworks
+      sample_authorize
     else
       #This is a regular show page we want the user to see, after the search
       @artwork = Artwork.find(params[:id])
@@ -26,4 +24,11 @@ class ArtworksController < ApplicationController
       end
     end
   end
+
+  private
+
+    def sample_authorize
+      @artwork = @artworks.sample
+      authorize @artworks
+    end
 end
