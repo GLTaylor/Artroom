ee9ae2e4a1225975f2da30471d0b03a98049032d
camille raiser <camille.raiser@hotmail.fr>
2018-08-22 17:49:40 +0200

    changed user authorization on show page

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -18,10 +18,6 @@ class ArtworksController < ApplicationController
       #This is a regular show page we want the user to see, after the search
       @artwork = Artwork.find(params[:id])
       authorize @artwork
-      if current_user.present?
-        @user = current_user
-        authorize @user
-      end
     end
   end
 
