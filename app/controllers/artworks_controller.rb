6f3efc6cc71cc2c7bce9fd4a0bba9e3cdae9c5dd
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-08-22 11:47:30 +0200

    Search box appears

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,11 +1,19 @@
 class ArtworksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
+
+
+
   def show
-    @artwork = Artwork.find(params[:id])
-    authorize @artwork
-    if current_user.present?
-      @user = current_user
-      authorize @user
-    end
+    # if params[:query].present?
+
+    # end
+    # else
+      # This is a regular show page we want the user to see, after the search
+      @artwork = Artwork.find(params[:id])
+      authorize @artwork
+      if current_user.present?
+        @user = current_user
+        authorize @user
+      end
   end
 end
