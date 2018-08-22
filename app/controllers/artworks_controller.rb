45bb7198fc66dd97c118b31df752dd097cfdad9b
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-08-22 12:44:29 +0200

    add pg search gem

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -4,10 +4,14 @@ class ArtworksController < ApplicationController
 
 
   def show
-    # if params[:query].present?
-
-    # end
-    # else
+    if params[:query].present?
+      sql_query = "\
+        artwork.mood @@ :query \
+        AND artwork.interest @@ :query \
+      "
+      @artwork = Artwork.find_by(sql_query, query: "%#{params[:query]}%")
+      authorize @artwork
+    else
       # This is a regular show page we want the user to see, after the search
       @artwork = Artwork.find(params[:id])
       authorize @artwork
@@ -15,5 +19,6 @@ class ArtworksController < ApplicationController
         @user = current_user
         authorize @user
       end
+    end
   end
 end
