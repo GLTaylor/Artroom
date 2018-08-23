0c922bf2b3254f8de75b66c442efeccc8e3d16f4
Noah Leal <noahleal@me.com>
2018-08-23 16:11:54 +0200

    adds route for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :artworks, only: [:show]
+  resources :artworks, only: [:index, :show]
   resources :users, only: [:show]
   resources :artists, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
