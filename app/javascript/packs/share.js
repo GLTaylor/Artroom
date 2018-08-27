519929c74bd4b03084bd3139f3e812d28ce282d1
camille raiser <camille.raiser@hotmail.fr>
2018-08-27 16:14:30 +0200

    comment js share out

--- app/javascript/packs/share.js
+++ app/javascript/packs/share.js
@@ -1,10 +1,10 @@
-const share = document.querySelector('.share');
-if (share) {
-setTimeout(() => {
-  share.classList.add("hover");
-}, 1000);
+// const share = document.querySelector('.share');
+// if (share) {
+// setTimeout(() => {
+//   share.classList.add("hover");
+// }, 1000);
 
-setTimeout(() => {
-  share.classList.remove("hover");
-}, 3000);
-}
+// setTimeout(() => {
+//   share.classList.remove("hover");
+// }, 3000);
+// }
